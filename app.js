9360380eda1da5a0204da547252fcae13d38b77c
Tri Sutrisno <tri.s@bts.id>
2025-02-08 14:36:26 +0700

    assigment middleware

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+// app.use((req, res, next) => {
+//     console.log('In the First middleware!');
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+
+// app.use((req, res, next) => {
+//     console.log('In second middleware!');
+//     res.send('<h1>Hello from Express!</h1>');
+// });
+
+app.use('/users', (req, res, next) => {
+    console.log('/ middleware!');
+    res.send('<h1>The "Users" Page</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.use('/', (req, res, next) => {
+    console.log('/ users middleware!');
+    res.send('<h1>The "Home" Page</h1>');
 });
 
 const server = http.createServer(app);
