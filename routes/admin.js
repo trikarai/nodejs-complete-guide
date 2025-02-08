d546ca4400d849740394decc32e8ca89a98eb975
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-08 22:19:24 +0700

    sharing data

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const rootDir = require('../utils/path.js');
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     /** 
@@ -14,8 +16,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const router = express.Router();    
 
 const rootDir = require('../utils/path.js');
+const adminData = require('./admin');   
 
 
 router.get('/', (req, res, next) => {
- res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log('shop.js', adminData.products);
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 
