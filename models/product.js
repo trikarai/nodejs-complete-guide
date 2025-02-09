531d62de858cd5d3c0430d9ab9de5735437c1820
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-09 18:54:20 +0700

    storing data in files

--- models/product.js
+++ models/product.js
@@ -1,15 +1,44 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product { 
+ module.exports = class Product { 
     constructor(title) {
         this.title = title;
     }
 
-    save() {
-        products.push(this);
+    save() { 
+        const p = path.join(
+            path.dirname(require.main.filename), 
+            'data', 
+            'products.json');
+
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                try {
+                    products = JSON.parse(fileContent);
+                } catch (parseErr) {
+                    console.log('Error parsing JSON:', parseErr);
+                }
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
-    }
+        const p = path.join(
+            path.dirname(require.main.filename), 
+            'data', 
+            'products.json');
+            
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
+     }
 }
