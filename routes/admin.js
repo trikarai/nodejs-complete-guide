1c444e67c5b8d1d7f151cd0982d2fb708bbb9a93
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-09 12:32:31 +0700

    working with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -8,12 +8,18 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product' });
- });
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+});
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title, price: req.body.price, description: req.body.description });
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,14 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
      const products = adminData.products;
-     res.render('shop', {prods: products, docTitle: 'Shop'});
-});
-
+     res.render('shop', {
+       prods: products,
+       pageTitle: 'Shop',
+       path: '/',
+       hasProducts: products.length > 0,
+       activeShop: true,
+       productCSS: true
+     });
+   });
 
 module.exports = router;
