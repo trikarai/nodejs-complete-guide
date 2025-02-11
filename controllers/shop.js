223ef2c6158dceb8d031bedb1a767bacb718c7b7
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-11 15:31:10 +0700

    feat: gettin single product sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,6 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-
   Product.findAll()
   .then(products => {
     res.render('shop/product-list', {
@@ -17,13 +16,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    }).catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
