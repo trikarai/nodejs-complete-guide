1a6ab1c463cfbb3774a875e533e3cc7c618772ec
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-11 15:51:44 +0700

    feat: delete product using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,6 +89,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
-};
+
+  Product.destroy({
+    where: {
+      id: prodId
+    }
+  })
+    .then(() => {
+      console.log('Product Deleted');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+ };
