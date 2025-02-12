353c4b56609def066c2523e87ce47becbcd73ead
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-12 13:40:41 +0700

    adding order model

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./utils/database');
 
+// Routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
@@ -21,6 +22,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,12 +48,16 @@ app.use(errorController.get404Page);
 
 const server = http.createServer(app);
 
+// Associations
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
 // .sync({ force: true })
