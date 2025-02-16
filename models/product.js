6868c3ebfac913d5882b1cb19810b2d2889552a4
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-16 16:09:24 +0700

    fix: update product mongodb

--- models/product.js
+++ models/product.js
@@ -2,18 +2,24 @@ const { ObjectId } = require('mongodb')
 const { getDB } = require('../utils/database')
 
 class Product {
-  constructor(title, price, imageUrl, description){
+  constructor(title, price, imageUrl, description, id){
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl
     this.description = description
+    this._id = new ObjectId(id)
   }
 
   save(){
-     const db = getDB();
-     return db.collection('products')
-      .insertOne(this)
-      .then((result) => {
+    const db = getDB();
+    let dbOp;
+    if (this._id) {
+      // update the product
+        dbOp = db.collection("products").updateOne({ _id: new ObjectId(this._id) }, { $set: this });
+      } else {
+        dbOp = db.collection("products").insertOne(this);
+      }
+      return dbOp.then((result) => {
         console.log(result) 
       }).catch((err) => {
         console.log(err)
