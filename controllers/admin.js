5c94d67e93840e4f3a2f39a97aa3f823694dce3d
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-22 20:06:46 +0700

    fix: update product deletion logic and enable delete product route

--- controllers/admin.js
+++ controllers/admin.js
@@ -98,7 +98,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log('Product Deleted');
       res.redirect('/admin/products');
