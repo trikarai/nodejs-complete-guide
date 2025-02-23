0cc558e2e01ab7b73ffe6bf4c8eff9c8c44b602a
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-02-23 08:53:44 +0700

    fix: update product retrieval logic to include userId population

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,6 +85,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price imageUrl -description')
+    // .populate('userId', name)
     .then(products => {
       res.render('admin/products', {
         prods: products,
