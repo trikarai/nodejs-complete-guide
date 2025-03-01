eb8f2ca6aad5ea645cec56bea4374ccc0751937c
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-03-01 20:21:33 +0700

    feat: update order model to use email instead of name for user identification

--- controllers/shop.js
+++ controllers/shop.js
@@ -92,7 +92,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: user.name,
+          email: user.email,
           userId: user
         },
         products: products
