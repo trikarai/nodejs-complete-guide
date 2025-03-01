d306852325d7edb56d19ae93913f6150cf1eef44
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-03-01 15:59:23 +0700

    feat: add signup functionality with routing and view integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById("67ba7bd70514035140f5d147")
     .then((user) => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
