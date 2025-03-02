831c5b8451aa65a2042412ebfff0938f8640cac4
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-03-02 17:12:41 +0700

    feat: implement password update functionality with token validation and error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -182,3 +182,32 @@ exports.getNewPassword = (req, res, next) => {
     console.log(err);
   });
 }
+
+exports.postNewPassword = (req, res, next) => { 
+  const newPassword = req.body.newPassword;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+    User.findOne({
+      resetToken: passwordToken,
+      resetTokenExpiration: { $gt: Date.now() },
+      _id: userId,
+    })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
--- routes/auth.js
+++ routes/auth.js
@@ -20,4 +20,6 @@ router.post("/reset", authController.postReset);
 
 router.get('/reset/:token', authController.getNewPassword);
 
+router.post('/reset-password', authController.postNewPassword);
+
 module.exports = router;
