319257babea3fb08ca1831d945cc8136de653913
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-05 16:45:40 +0700

    feat: implement pagination for product listing with navigation controls and total items display

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,12 +6,35 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
+
+  const page = req.query.page || 1;
+  const ITEM_PER_PAGE = 2;
+  let startIndex = (page - 1) * ITEM_PER_PAGE;
+  let totalItems = 0;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = +numProducts;
+      return Product.find().skip(startIndex).limit(ITEM_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
-        path: "/",
+        path: "/products",
+        meta: {
+          totalItems: totalItems,
+          currentPage: +page,
+          firstPage: 1,
+          hasNextPage: ITEM_PER_PAGE * page < totalItems,
+          hasPreviousPage: parseInt(page) > 1,
+          nextPage: parseInt(page) + 1,
+          previousPage: parseInt(page) - 1,
+          lastPage: Math.ceil(totalItems / ITEM_PER_PAGE),
+          totalPages: Math.ceil(totalItems / ITEM_PER_PAGE),
+        },
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -49,7 +72,6 @@ exports.getIndex = (req, res, next) => {
     .find()
     .countDocuments()
     .then((numProducts) => {
-			console.log("TCL: exports.getIndex -> numProducts", numProducts)
       totalItems = +numProducts;
       return Product.find()
         .skip(startIndex)
