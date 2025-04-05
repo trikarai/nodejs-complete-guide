99267f4435200617bd37fe394ec7d9201ca6fbfb
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-05 08:55:36 +0700

    fix: improve error page messaging and ensure authentication check is safe

--- controllers/error.js
+++ controllers/error.js
@@ -10,6 +10,6 @@ exports.get500Page = (req, res, next) => {
   res.status(500).render("500", {
     pageTitle: "Error",
     path: "/500",
-    isAuthenticated: req.session.isLoggedIn,
+    isAuthenticated: !!req.session?.isLoggedIn,
   });
 };
