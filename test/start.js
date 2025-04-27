f97cb2119f06917cbd49f3fc0bdf742b8c74ad34
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-27 17:21:27 +0700

    feat: add initial test cases for addition and subtraction

---
+++ test/start.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+it('should add numbers correctly', () => {
+  const result = 1 + 2;
+  expect(result).to.equal(3);
+});
+it('should subtract numbers correctly', () => {
+  const result = 5 - 2;
+  expect(result).to.equal(3);
+});
